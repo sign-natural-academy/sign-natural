9998fdd13e3f4654298e2147dc052970baaabdae
Sam Quaye <samuelmensahquaye@gmail.com>
2025-05-07 03:39:06 +0000

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,4 @@
-// import { useState } from 'react'
-// import './App.css'
-// import { Footer } from './components/Footer'
-// import Homepage from './components/Homepage'
-// import Navbar from './components/Navbar'
 
-
-// function App() {
-
-//   return (
-//     <>
-//     <Homepage/>
-//     <Footer/>
-//     </>
-//   )
-// }
-
-// export default App
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
